ec09850889c4659932dfbdf61d52142441a0df39
Darren Smith <dxsmith12@gmail.com>
2020-03-02 21:35:22 -0500

    edited production.py

--- education_portal/education_portal/wsgi.py
+++ education_portal/education_portal/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.dev")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.production")
 
 application = get_wsgi_application()
