5b97f68e263148523a1621043fecb0c182276793
Darren Smith <dxsmith12@gmail.com>
2020-03-08 20:21:05 -0400

    commented facebook image error from sentry

--- education_portal/education_portal/wsgi.py
+++ education_portal/education_portal/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.dev")
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.production")
+#  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_portal.settings.production")
 
 application = get_wsgi_application()
